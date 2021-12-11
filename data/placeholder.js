9eefa11fe489e836d18265f79f156c5d4ee00624
Philippe Allard-Rousse <philrousse@gmail.com>
2021-12-11 03:54:02 -0500

    WEB2-10: Creation de la class

--- data/placeholder.js
+++ data/placeholder.js
@@ -1,1 +1,14 @@
 //Endroit pour créé nos données placeholder en attendant d'integrer les vrais données
+
+export let ph_MeteoItem = {
+    "location": {
+        "name": "Montreal"
+    },
+    "current": {
+        "temp_c": -4.6,
+        "condition": {
+            "icon": "//cdn.weatherapi.com/weather/64x64/night/113.png"
+        },
+        "feelslike_c": -7
+    }
+};
