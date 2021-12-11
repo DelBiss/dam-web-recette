76f29fe1b06abe3afd105f37feac432b781c93ea
Philippe Allard-Rousse <philrousse@gmail.com>
2021-12-11 03:54:23 -0500

    Integration dans la vitrine

--- components/loadComponents.js
+++ components/loadComponents.js
@@ -1,1 +1,6 @@
 import * as Utils from "../script/utils.js";
+
+import MeteoItem from "./meteo/class.js";
+Utils.LoadCSS('./meteo/style.css').then(function() {
+    Utils.renderComponentOnBody(MeteoItem);
+});
