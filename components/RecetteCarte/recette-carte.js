c756a134b0452585bce5e83776d26922887dc675
Philippe Allard-Rousse <philrousse@gmail.com>
2021-12-12 14:55:34 -0500

    Component Class implemented

--- components/RecetteCarte/recette-carte.js
+++ components/RecetteCarte/recette-carte.js
@@ -1,2 +1,10 @@
 // méthode pour afficher les éléments recettes
-Constructor() 
+import { ph_Recettes } from "../../data/placeholder.js";
+import Component from "../cComponent/Component.js";
+
+export default class RecetteCarte extends Component {
+    constructor(props) {
+        super(props, "data-recette-detail");
+        this.props = props ? props : ph_Recettes[0];
+    }
+}
--- components/loadComponents.js
+++ components/loadComponents.js
@@ -1,6 +1,8 @@
 import * as Utils from "../script/utils.js";
-import { ph_MeteoItem } from "../data/placeholder.js";
+import { ph_MeteoItem, ph_Recettes } from "../data/placeholder.js";
 
 import MeteoItem from "./MeteoItem/class.js";
+import RecetteCarte from "./RecetteCarte/recette-carte.js"
 
 Utils.renderComponentInShowcase(MeteoItem, ph_MeteoItem)
+Utils.renderComponentInShowcase(RecetteCarte, ph_Recettes)
