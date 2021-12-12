9b6cd10a00b6768982796d9d06e92f9d2467e721
Philippe Allard-Rousse <philrousse@gmail.com>
2021-12-12 14:05:44 -0500

    Simplified showcase in Viterine

--- components/loadComponents.js
+++ components/loadComponents.js
@@ -1,15 +1,16 @@
 import * as Utils from "../script/utils.js";
 import { ph_MeteoItem } from "../../data/placeholder.js";
-import { getDatasetKeyFromSelector } from "../script/utils.js";
+
 import MeteoItem from "./MeteoItem/class.js";
 // Utils.LoadCSS('./meteo/style.css').then(function() {
 //     Utils.renderComponentOnBody(MeteoItem);
 // });
 
-const meteo1 = new MeteoItem(ph_MeteoItem[0])
-const meteo2 = new MeteoItem(ph_MeteoItem[1])
-await meteo1.render(document.body);
-await meteo2.render(document.body);
+Utils.renderComponentInShowcase(MeteoItem, ph_MeteoItem)
+    // const meteo1 = new MeteoItem(ph_MeteoItem[0])
+    // const meteo2 = new MeteoItem(ph_MeteoItem[1])
+    // await meteo1.render(document.body);
+    // await meteo2.render(document.body);
 
 // let el = await meteo.render();
 // console.log(meteo)
