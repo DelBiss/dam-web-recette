5e27f440f2db700798220e82a2aa4f1fe2d7b5ba
Philippe Allard-Rousse <philrousse@gmail.com>
2021-12-12 12:56:43 -0500

    Simplified Componenet Object

--- components/loadComponents.js
+++ components/loadComponents.js
@@ -1,12 +1,16 @@
 import * as Utils from "../script/utils.js";
 import { ph_MeteoItem } from "../../data/placeholder.js";
-
+import { getDatasetKeyFromSelector } from "../script/utils.js";
 import MeteoItem from "./MeteoItem/class.js";
 // Utils.LoadCSS('./meteo/style.css').then(function() {
 //     Utils.renderComponentOnBody(MeteoItem);
 // });
-document.body.appendChild(await new MeteoItem(ph_MeteoItem[0]).render());
-document.body.appendChild(await new MeteoItem(ph_MeteoItem[1]).render());
+
+const meteo1 = new MeteoItem(ph_MeteoItem[0])
+const meteo2 = new MeteoItem(ph_MeteoItem[1])
+await meteo1.render(document.body);
+await meteo2.render(document.body);
+
 // let el = await meteo.render();
 // console.log(meteo)
 // console.log(el)
