c9ae60155e60222d0a9307b75afeb049b9d9587f
Philippe Allard-Rousse <philrousse@gmail.com>
2021-12-15 17:48:20 -0500

    Skeleton for Alain

---
+++ script/search.js
@@ -0,0 +1,10 @@
+import { ph_Recettes } from "../data/placeholder.js"
+
+function search(searchTerm, isCategories = false) {
+    data = ph_Recettes;
+    //////////////////////
+    //forof //loop tableau
+    //forin //loop object
+}
+
+//texte instanceof Array
