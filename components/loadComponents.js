81f9af521b5007a84a1624a7186a643911cf212b
AlainBernier <AlainBernier>
2021-12-15 17:58:17 -0500

    modif de nom

--- components/loadComponents.js
+++ components/loadComponents.js
@@ -1,8 +1,9 @@
 import * as Utils from "../script/utils.js";
 import { ph_MeteoItem, ph_Recettes } from "../data/placeholder.js";
-
+import RecetteDetail from "./recetteDetail/recetteDetail.js"
 import MeteoItem from "./MeteoItem/class.js";
 import RecetteCarte from "./RecetteCarte/recette-carte.js"
 
 Utils.renderComponentInShowcase(MeteoItem, ph_MeteoItem)
 Utils.renderComponentInShowcase(RecetteCarte, ph_Recettes)
+Utils.renderComponentInShowcase(RecetteDetail, ph_Recettes)
