4e3f0f185676504f5d6003ebbede8243e974565e
Philippe Allard-Rousse <philrousse@gmail.com>
2021-12-16 15:31:05 -0500

    update

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 import { ph_MeteoItem, ph_Recettes } from "./data/placeholder.js";
 import MeteoItem from "./components/MeteoItem/class.js";
 import RecetteLivre from "./components/RecetteLivre/class.js";
+import Recherche from "./components/Recherche/class.js";
+import Navigation from "./components/Navigation/class.js";
 
 let meteoMtl = new MeteoItem(ph_MeteoItem[0])
 let livre = new RecetteLivre(ph_Recettes);
+let recherche = new Recherche();
+let nav = new Navigation();
 
 await meteoMtl.render("meteo")
-
 await livre.render("livre")
+await recherche.render("search")
+await nav.render("nav")
 
 meteoMtl.addClickEvent(meteoMtl.event_RotateData())
--- components/Recherche/class.js
+++ components/Recherche/class.js
@@ -1,10 +1,9 @@
 //@ts-check
 import Component from "../cComponent/Component.js";
 
-export default class Navigation extends Component {
+export default class Recherche extends Component {
     constructor(props) {
         super(props, "data-recherche");
 
     }
-
 }
